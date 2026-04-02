a788baeb1f08146e643a7fbabd5028f43f4e857e
Kyeonglim99 <kklim99@hufs.ac.kr>
2026-04-02 17:14:45 +0900

    SY2026-39-RouterUpdate

--- src/router/Web_index.jsx
+++ src/router/Web_index.jsx
@@ -6,6 +6,7 @@ import Web_WipListPage from "../pages/office/Web_WipListPage/Web_WipListPage";
 import Web_LantekInputPage from "../pages/office/Web_LantekInputPage/Web_LantekInputPage";
 import WorkOrderPage from "../pages/office/WorkOrderPage/WorkOrderPage";
 import ScenarioPage from "../pages/office/ScenarioPage/ScenarioPage";
+import Web_ScenarioResultPage from "../pages/office/Web_ScenarioResultPage/Web_ScenarioResultPage";
 
 const webRoutes = [
   {
@@ -14,6 +15,7 @@ const webRoutes = [
       { path: "dashboard", element: <DashboardPage /> },
       { path: "wip", element: <Web_WipListPage /> },
       { path: "scenario/input", element: <Web_LantekInputPage /> },
+      { path: "scenario/result", element: <Web_ScenarioResultPage /> },
       { path: "work-order", element: <WorkOrderPage /> },
       { path: "scenario", element: <ScenarioPage /> },
     ],
