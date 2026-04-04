bfcf32e46e675b030ba6237e8dd94d9bf8aa6bd6
김나연 <nayoen020211@gmail.com>
2026-04-05 03:03:14 +0900

    SY2026-55-PickingRawQrPage

--- src/router/App_index.jsx
+++ src/router/App_index.jsx
@@ -8,7 +8,7 @@ import App_RelocateQrZonePage from "../pages/field/RelocateQrZonePage/App_Reloca
 import App_PickingWipPage from "../pages/field/PickingWipPage/App_PickingWipPage";
 import App_PickingWipQrPage from "../pages/field/PickingWipQrPage/App_PickingWipQrPage";
 import App_PickingRawPage from "../pages/field/PickingRawPage/App_PickingRawPage";
-// import App_PickingRawQrPage from "../pages/field/PickingRawQrPage/App_PickingRawQrPage";
+import App_PickingRawQrPage from "../pages/field/PickingRawQrPage/App_PickingRawQrPage";
 // import App_ProcessingPage from "../pages/field/ProcessingPage/App_ProcessingPage";
 // import App_ProcessingQrPage from "../pages/field/ProcessingQrPage/App_ProcessingQrPage";
 // import App_EndPage from "../pages/field/EndPage/App_EndPage";
@@ -30,7 +30,7 @@ const appRoutes = [
       { path: "ready/picking/wip/qr", element: <App_PickingWipQrPage /> },
 
       { path: "ready/picking/raw", element: <App_PickingRawPage /> },
-      // { path: "ready/picking/raw/qr", element: <App_PickingRawQrPage /> },
+      { path: "ready/picking/raw/qr", element: <App_PickingRawQrPage /> },
 
       // { path: "processing", element: <App_ProcessingPage /> },
       // { path: "processing/qr", element: <App_ProcessingQrPage /> },
